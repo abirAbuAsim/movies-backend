22b243cd6fb1c571951e7f4cc8550195de02478a
Abir Khan <abir.sakif@gmail.com>
2022-08-29 10:59:56 +0600

    Added typeorm and postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'movies-db',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
