bdb2de66b040367063c0506ec7be99410cc17f33
Abir Khan <abir.sakif@gmail.com>
2022-08-29 09:57:59 +0600

    Added global pipe

--- src/movies/dto/get-movies-filter.dto.ts
+++ src/movies/dto/get-movies-filter.dto.ts
@@ -1,6 +1,10 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { MOVIE_GENRE } from "../movie-genre.enum";
 
 export class GetMoviesFilterDto {
+    @IsOptional()
+    @IsString()
     name?: string;
+
     genre?: MOVIE_GENRE
 }
