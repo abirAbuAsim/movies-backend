33459f301aefdf3f8e7e1362f50634aee53e44b1
Abir Khan <abir.sakif@gmail.com>
2022-08-30 09:53:06 +0600

    Added signup and signin endpoints

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,28 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { UsersRepository } from './movies.repository';
+import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: 'B[T@_6_-M2ux\^u),<7D9hsu99x.2-}bX_2bUXgnW?#5YT*cn$d{HjvBW^#Jfs]j',
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   providers: [AuthService, UsersRepository],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService, PassportModule]
 })
 export class AuthModule {}
