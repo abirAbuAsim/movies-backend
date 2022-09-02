372035927efbcf08bd1e398c7bc2719d9004dd47
Abir Khan <abir.sakif@gmail.com>
2022-09-02 10:24:42 +0600

    enabled cors for api

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const port = 3000;
+  const port = 3001;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
